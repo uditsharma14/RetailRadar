0ca6b557da874837a14316eccdfd2225e9a8dd13
uditsharma <Udsha#232425>
2024-12-05 10:32:59 +0530

    added service to query the product based on query

--- RetailRadarApp.py
+++ RetailRadarApp.py
@@ -13,8 +13,8 @@ def search():
     # Retrieve query parameters
     query = request.args.get('query')
     category = request.args.get('category')
-    if not query or not category:
-        return jsonify({"error": "Missing required parameters: query and category"}), 400    
+    if not query :
+        return jsonify({"error": "Missing required parameters: query "}), 400    
     # Fetch results based on the category
     results = search_handler(query)
     return results
