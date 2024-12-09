5685d5a1c25fa0ad6e72236664bd10100a1d9f3d
uditsharma <Udsha#232425>
2024-12-09 23:23:39 +0530

    added ors

--- RetailRadarApp.py
+++ RetailRadarApp.py
@@ -1,14 +1,20 @@
 from AmazonDataProcessing import prepare_data
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+
 
 from AmazonDataProcessing import search_handler
 
 app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+CORS(app)
 
 def initialize():
     prepare_data()
 
+
 @app.route('/search', methods=['GET'])
+@cross_origin()
 def search():
     # Retrieve query parameters
     query = request.args.get('query')
